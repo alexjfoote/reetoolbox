e4c216d612e838cad5873f706e61620ddabac328
alexjfoote <alex.foote@ripjar.com>
2022-01-17 16:16:21 +0000

    Update dependency versions

--- setup.py
+++ setup.py
@@ -2,15 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='reetoolbox',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(include=['reetoolbox', 'reetoolbox.*']),
     install_requires=[
         'numpy==1.19.5',
         'scikit-learn==1.0.2',
-        'torch==1.10.0+cu111',
-        'torchvision==0.11.1+cu111',
-        'matplotlib==3.2.2',
+        'torch==1.10.0',
+        'torchvision==0.11.1',
+        'matplotlib',
         'seaborn==0.11.2',
-        'Pillow==7.1.2'
-    ]
+        'Pillow'
+    ],
+    author='Alex Foote',
+    author_email='alexjfoote@icloud.com',
+    url="https://github.com/alexjfoote/reetoolbox",
 )
